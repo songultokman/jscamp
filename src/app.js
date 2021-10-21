c3a79980ccb701733e0897153001a53f63720fde
songultokman <songultkmn@gmail.com>
2021-10-22 00:41:23 +0300

    18 ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,22 @@
+console.log("Merhaba benim adım name")
+
+console.log("merhabaa canım")
+
+let dolarDun=9.30
+
+let dolarBugun=9.20
+{
+    let dolarDun=9.1
+}
+console.log(dolarDun)
+
+let konutKredileri=["Konut kredisi", "Emlak kredisi", "Kamu konut kredisi"]
+console.log("<ul>")
+for(let i=0; i<konutKredileri.length;i++)
+{
+    console.log("<li>"+ konutKredileri[i])
+}
+console.log("</ul>")
+
+
+
